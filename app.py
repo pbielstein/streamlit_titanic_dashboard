6569c0d43e7db08cebf691dbadf1bf16e8cf8a34
pbielstein <patrick.bielstein@gmail.com>
2025-08-11 14:24:34 +0100

    changes to layout

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title('Titanic Dashboard')
+set.set_page_config(page_title='Titanic Dashboard', layout='wide')
+#st.title('Titanic Dashboard')
+
+df = pd.read_csv('titanic.csv')
+
+df['Embarked'] = df['Embarked'].fillna('Unkown')
+
+# get unique values for Embarked
+embarked_port = df['Embarked'].unique().tolist()
+gender = df['Sex'].unique().tolist()
+
+col1, col2 = st.columns([1, 1])
+selected_port = col1.selectbox(options=embarked_port, label='Select a port')
+selected_gender = col2.selectbox(options=gender, label='Select a gender')
