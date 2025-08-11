eb91a9ad16ccf856a40315ee8a06f7fd0edb5b48
pbielstein <patrick.bielstein@gmail.com>
2025-08-11 14:26:38 +0100

    bugfix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-set.set_page_config(layout='wide')
+st.set_page_config(layout='wide')
 st.title('Titanic Dashboard')
 
 df = pd.read_csv('titanic.csv')
